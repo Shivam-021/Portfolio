6a19474d66b73af5c22e9d5bb47ea4aea0cd9afe
Shivam-021 <shivamsharma21022003@gmail.com>
2025-06-27 16:58:35 +0530

    again in js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing text
 var typed = new Typed(".typing",{
-    strings:["Web Developer", "MERN Techie", "Full Stack Developer", "Software Engineer"],
+    strings:["Web Developer", "MERN Techie",   "Software Engineer"],
     typespeed:40,
     BackSpeed:40,
     loop:true,
